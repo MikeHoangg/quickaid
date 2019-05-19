a1dddf8261bedca064ec1a492327f56fb448533f
Mike Hoang <hoang.viet.ha.business@gmail.com>
2019-05-20 02:42:08 +0300

    Altered models. Added new model for Notifications

--- medicine/models.py
+++ medicine/models.py
@@ -3,6 +3,12 @@ from django.utils.translation import gettext as _
 from multiselectfield import MultiSelectField
 
 
+class PatchedMultiSelectField(MultiSelectField):
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        return self.get_prep_value(value)
+
+
 class Statistics(models.Model):
     min_heart_rate = models.PositiveIntegerField(verbose_name=_('min heart rate'))
     max_heart_rate = models.PositiveIntegerField(verbose_name=_('max heart rate'))
@@ -13,7 +19,7 @@ class Statistics(models.Model):
     ferritin_rate = models.FloatField(verbose_name=_('ferritin rate'))
     cholesterol_rate = models.FloatField(verbose_name=_('cholesterol rate'))
     temperature = models.FloatField(verbose_name=_('temperature'))
-    created = models.DateTimeField(verbose_name='created', auto_created=True, editable=False)
+    created = models.DateTimeField(verbose_name='created', auto_now_add=True, editable=False)
     user = models.ForeignKey(to='core.User', on_delete=models.CASCADE)
 
     class Meta:
@@ -23,7 +29,7 @@ class Statistics(models.Model):
 
 class Diagnosis(models.Model):
     verdict = models.TextField(verbose_name=_('verdict'))
-    created = models.DateTimeField(verbose_name='created', auto_created=True, editable=False)
+    created = models.DateTimeField(verbose_name='created', auto_now_add=True, editable=False)
     statistics = models.ForeignKey(to='medicine.Statistics', on_delete=models.CASCADE)
 
     class Meta:
@@ -56,10 +62,16 @@ class Schedule(models.Model):
     description = models.TextField(verbose_name=_('description'))
     expiration_date = models.DateField(verbose_name=_('expiration date'), null=True, blank=True)
     time = models.TimeField(verbose_name=_('time'))
-    days = MultiSelectField(verbose_name=_('days'), choices=DAYS_CHOICES, min_choices=1)
+    days = PatchedMultiSelectField(verbose_name=_('days'), choices=DAYS_CHOICES, min_choices=1)
     active = models.BooleanField(verbose_name=_('active'), default=True)
-    user = models.ForeignKey(to='core.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(verbose_name=_('user'), to='core.User', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _('schedule')
         verbose_name_plural = _('schedules')
+
+
+class Notification(models.Model):
+    schedule = models.ForeignKey(verbose_name=_('schedule'), to='medicine.Schedule', on_delete=models.CASCADE)
+    created = models.DateTimeField(verbose_name='created', auto_now_add=True, editable=False)
+    pending = models.BooleanField(verbose_name=_('pending'), default=True)
