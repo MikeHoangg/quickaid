f0d74cb191efdedeb4df09112f757e81827dd5db
Mike Hoang <hoang.viet.ha.business@gmail.com>
2019-05-20 02:32:32 +0300

    Updated consumer for web sockets

--- core/consumers.py
+++ core/consumers.py
@@ -1,16 +1,42 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+from medicine.models import Notification
+from medicine.serializers import NotificationSerializer
 
-class NotificationConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
 
-    def disconnect(self, close_code):
-        pass
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        self.room_name = f"notifications_for_user_{self.user_id}"
 
-    def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        self.send(text_data=json.dumps({
-            text_data_json
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data=None, bytes_data=None):
+        notifications = NotificationSerializer(
+            Notification.objects.filter(schedule__user=self.user_id, pending=True)).data
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                'type': 'chat_message',
+                'notifications': notifications
+            }
+        )
+
+    async def chat_message(self, event):
+        notifications = event['notifications']
+
+        await self.send(text_data=json.dumps({
+            'notifications': notifications
         }))
--- core/routing.py
+++ core/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('notifications/', consumers.NotificationConsumer),
+    path('notifications/<int:user_id>', consumers.NotificationConsumer),
 ]
