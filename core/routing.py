e88dc4b6fc114e4096e52dd1f231fd2000cb6b52
Mike Hoang <hoang.viet.ha.business@gmail.com>
2019-05-20 02:32:57 +0300

    Updated routing for web sockets

--- core/routing.py
+++ core/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('notifications/<int:user_id>', consumers.NotificationConsumer),
+    path('notifications/<int:user_id>/', consumers.NotificationConsumer),
 ]
