8466f9c7183b8bf4d6b223676cca0f8d97ca1e85
Mike Hoang <hoang.viet.ha.business@gmail.com>
2019-05-19 17:50:46 +0300

    Added new app for medicine

---
+++ medicine/constants.py
@@ -0,0 +1,148 @@
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+
+User = get_user_model()
+
+HEART_RATE_UNIT = _('mm Hg')
+GLUCOSE_RATE_UNIT = CHOLESTEROL_RATE_UNIT = _('mmol/l')
+TEMPERATURE_UNIT = _('°C')
+PROTEIN_RATE_UNIT = ALBUMIN_RATE_UNIT = _('g/l')
+MYOGLOBIN_RATE_UNIT = FERRITIN_RATE_UNIT = _('mcg/l')
+
+TEMPERATURE_NORM = 36.6
+
+HEART_RATE_NORM = {
+    User.MALE: {
+        20: {
+            'norm_min': 76,
+            'norm_max': 123
+        },
+        30: {
+            'norm_min': 79,
+            'norm_max': 126
+        },
+        40: {
+            'norm_min': 81,
+            'norm_max': 129
+        },
+        50: {
+            'norm_min': 83,
+            'norm_max': 135
+        },
+        60: {
+            'norm_min': 85,
+            'norm_max': 142
+        },
+        float('inf'): {
+            'norm_min': 80,
+            'norm_max': 142
+        }
+    },
+    User.FEMALE: {
+        20: {
+            'norm_min': 72,
+            'norm_max': 116
+        },
+        30: {
+            'norm_min': 75,
+            'norm_max': 120
+        },
+        40: {
+            'norm_min': 80,
+            'norm_max': 127
+        },
+        50: {
+            'norm_min': 84,
+            'norm_max': 137
+        },
+        60: {
+            'norm_min': 85,
+            'norm_max': 144
+        },
+        float('inf'): {
+            'norm_min': 85,
+            'norm_max': 159
+        }
+    }
+
+}
+
+GLUCOSE_RATE = {
+    14: {
+        'norm_min': 3.3,
+        'norm_max': 5.6
+    },
+    60: {
+        'norm_min': 4.1,
+        'norm_max': 5.9
+    },
+    90: {
+        'norm_min': 4.6,
+        'norm_max': 6.4
+    },
+    float('inf'): {
+        'norm_min': 4.2,
+        'norm_max': 6.7
+    }
+}
+
+PROTEIN_RATE = {
+    4: {
+        'norm_min': 64,
+        'norm_max': 75
+    },
+    7: {
+        'norm_min': 52,
+        'norm_max': 78
+    },
+    15: {
+        'norm_min': 58,
+        'norm_max': 76
+    },
+    float('inf'): {
+        'norm_min': 64,
+        'norm_max': 83
+    }
+}
+
+ALBUMIN_RATE = {
+    14: {
+        'norm_min': 28,
+        'norm_max': 54
+    },
+    60: {
+        'norm_min': 35,
+        'norm_max': 50
+    },
+    float('inf'): {
+        'norm_min': 34,
+        'norm_max': 48
+    }
+}
+
+MYOGLOBIN_RATE = {
+    User.MALE: {
+        'norm_min': 19,
+        'norm_max': 92
+    },
+    User.FEMALE: {
+        'norm_min': 12,
+        'norm_max': 76
+    }
+}
+
+FERRITIN_RATE = {
+    User.MALE: {
+        'norm_min': 25,
+        'norm_max': 250
+    },
+    User.FEMALE: {
+        'norm_min': 10,
+        'norm_max': 120
+    }
+}
+
+CHOLESTEROL_RATE = {
+    'norm_min': 3,
+    'norm_max': 6
+}
---
+++ medicine/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.utils.translation import gettext as _
+from multiselectfield import MultiSelectField
+
+
+class Statistics(models.Model):
+    min_heart_rate = models.PositiveIntegerField(verbose_name=_('min heart rate'))
+    max_heart_rate = models.PositiveIntegerField(verbose_name=_('max heart rate'))
+    glucose_rate = models.FloatField(verbose_name=_('glucose rate'))
+    protein_rate = models.FloatField(verbose_name=_('protein rate'))
+    albumin_rate = models.FloatField(verbose_name=_('albumin rate'))
+    myoglobin_rate = models.FloatField(verbose_name=_('myoglobin rate'))
+    ferritin_rate = models.FloatField(verbose_name=_('ferritin rate'))
+    cholesterol_rate = models.FloatField(verbose_name=_('cholesterol rate'))
+    temperature = models.FloatField(verbose_name=_('temperature'))
+    created = models.DateTimeField(verbose_name='created', auto_created=True, editable=False)
+    user = models.ForeignKey(to='core.User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _('statistics')
+        verbose_name_plural = _('statistics')
+
+
+class Diagnosis(models.Model):
+    verdict = models.TextField(verbose_name=_('verdict'))
+    created = models.DateTimeField(verbose_name='created', auto_created=True, editable=False)
+    statistics = models.ForeignKey(to='medicine.Statistics', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _('diagnosis')
+        verbose_name_plural = _('diagnosis')
+
+
+class Schedule(models.Model):
+    MONDAY = 'mon'
+    TUESDAY = 'tue'
+    WEDNESDAY = 'wed'
+    THURSDAY = 'thu'
+    FRIDAY = 'fri'
+    SATURDAY = 'sat'
+    SUNDAY = 'sun'
+    EVERYDAY = 'evr'
+
+    DAYS_CHOICES = (
+        (MONDAY, 'monday'),
+        (TUESDAY, 'tuesday'),
+        (WEDNESDAY, 'wednesday'),
+        (THURSDAY, 'thursday'),
+        (FRIDAY, 'friday'),
+        (SATURDAY, 'saturday'),
+        (SUNDAY, 'sunday'),
+        (EVERYDAY, 'everyday'),
+    )
+
+    reason = models.TextField(verbose_name=_('reason'), null=True, blank=True)
+    description = models.TextField(verbose_name=_('description'))
+    expiration_date = models.DateField(verbose_name=_('expiration date'), null=True, blank=True)
+    time = models.TimeField(verbose_name=_('time'))
+    days = MultiSelectField(verbose_name=_('days'), choices=DAYS_CHOICES, min_choices=1)
+    active = models.BooleanField(verbose_name=_('active'), default=True)
+
+    class Meta:
+        verbose_name = _('schedule')
+        verbose_name_plural = _('schedules')
